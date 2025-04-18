1885e5b50d0727e370cf7d93489e016a08b1a1b8
Eaviyi <eavidy@qq.com>
2025-04-18 16:14:40 +0800

    chore: publish

--- scripts/rewrite-deps.ts
+++ scripts/rewrite-deps.ts
@@ -1,5 +1,5 @@
 import path from "node:path";
 const file = path.join(import.meta.dirname!, "../deno.json");
 const c = await Deno.readTextFile(file).then((text) => JSON.parse(text));
-c.imports.hono = c.imports.hono.replace("npm:", "jsr:");
+c.imports.hono = c.imports.hono.replace(/^npm:hono"/, "jsr:@hono/hono");
 await Deno.writeTextFile("deno.json", JSON.stringify(c, null, 2));
